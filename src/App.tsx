8e57ac5d547a2a8481502a8fc66ea2866d7e82b4
Elli <nathan@cypherlab.org>
2026-02-02 10:47:25 +0100

    rm -dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Header from "./components/Header";
 import Dashboard from "./components/Dashboard";
 import Miners from "./components/Miners";
